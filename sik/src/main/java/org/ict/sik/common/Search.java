12414747c77801f7cdd3aa85d157556e56ea0015
JS Kim <kiqbbq0602@gmail.com>
2023-10-16 20:59:03 +0900

    브랜드 공지 검색기능 추가
    추가했음다

--- sik/src/main/java/org/ict/sik/common/Search.java
+++ sik/src/main/java/org/ict/sik/common/Search.java
@@ -8,10 +8,19 @@ public class Search {
 	private int endRow;   //페이지에 출력할 끝행
 	private Date begin;
 	private Date end;
-	private int age;	//나이 검색시 필요
+	private int age;	//나이 검색 시 필요
+	private String brand; // 브랜드 검색 시 필요
 	
 	
 	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
 	public int getAge() {
 		return age;
 	}
@@ -65,8 +74,10 @@ public class Search {
 	@Override
 	public String toString() {
 		return "Search [keyword=" + keyword + ", startRow=" + startRow + ", endRow=" + endRow + ", begin=" + begin
-				+ ", end=" + end + ", age=" + age + "]";
+				+ ", end=" + end + ", age=" + age + ", brand=" + brand + "]";
 	}
+
+	
 
 	
 		
--- sik/src/main/java/org/ict/sik/notice/controller/NoticeController.java
+++ sik/src/main/java/org/ict/sik/notice/controller/NoticeController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.ict.sik.brand.model.service.BrandService;
 import org.ict.sik.brand.model.vo.Brand;
 import org.ict.sik.common.Paging;
+import org.ict.sik.common.Search;
 import org.ict.sik.notice.model.service.NoticeService;
 import org.ict.sik.notice.model.vo.BrandNotice;
 import org.slf4j.Logger;
@@ -12,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,23 +24,52 @@ public class NoticeController {
 	private BrandService brandService;
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
 	@RequestMapping("brandNoticeListView.do")
-	public ModelAndView noticeListViewMethod(@RequestParam("page") String page,ModelAndView mv) {
+	public ModelAndView noticeListViewMethod(@RequestParam("page") String page,
+			@RequestParam(name = "brand", required = false) String brand,
+			@RequestParam(name = "keyword", required = false) String keyword,
+			ModelAndView mv) {
+		logger.info("넘어온값 확인 : { brand : " + brand + ", page :" + page+", keyword : " + keyword+"}");
 		int currentPage = 1;
 		if (page != null) {
 			currentPage = Integer.parseInt(page);
 		}
-
 		// 한 페이지 공지 10개 씩 출력되게 한다면
 		int limit = 10;
-
 		// 총 페이지 수 계산을 위한 공지글 총 갯수 조회
-		int listCount = noticeService.selectListCount();
+		Search search = new Search();
+		
+		
+		
+		int listCount = 0;
+		if(brand == null || brand.equals("all")) {
+			if(keyword == null) {
+				listCount = noticeService.selectListCount();
+			}else {
+				search.setKeyword(keyword);
+				listCount = noticeService.selectListCount(search);
+			}
+		}else {
+			search.setBrand(brand); // 브랜드 명으로 브랜드 아이디 가져와야함
+			if(keyword == null) {
+				listCount = noticeService.selectListCount(search);
+			}else {
+				search.setKeyword(keyword);
+				listCount = noticeService.selectListCount(search);
+			}
+		}
+		
 		Paging paging = new Paging(listCount, currentPage, limit, "selectBrandList.do");
 		paging.calculator();
-
+		search.setStartRow(paging.getStartRow());
+		search.setEndRow(paging.getEndRow());
+		
 		// 페이지에 출력할 목록 조회해 옴
-		ArrayList<BrandNotice> list = noticeService.selectList(paging);
+		ArrayList<BrandNotice> list = noticeService.selectList(search);
+		
+		
 		ArrayList<Brand> brandList = brandService.selectList();
 		if (list != null && list.size() > 0) {
 			mv.addObject("listCount", listCount);
@@ -54,16 +83,6 @@ public class NoticeController {
 			mv.addObject("message", "브랜드 리스트를 읽어오지 못했습니다.");
 			mv.setViewName("common/error");
 		}
-		return mv;
-	}
-	
-	@RequestMapping(value="searchBrandNoticeName.do", method=RequestMethod.POST)
-	public ModelAndView searchBrandNoticeNameMethod(ModelAndView mv,
-			@RequestParam("page") String page,
-			@RequestParam("brand") String brand,
-			@RequestParam("keyword") String keyword) {
-		logger.info("넘어온값 확인 : { brand : " + brand + ", page :" + page+", keyword : " + keyword+"}");
-		mv.setViewName("main");
 		return mv;
 	}
 }
