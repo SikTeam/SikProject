a4e57d386c8e0c5a999a1d1307a6d5907cefc9a7
scgonzo21 <gonzo19924@gmail.com>
2023-10-17 15:05:23 +0900

    1017 ing
    ing

--- sik/src/main/java/org/ict/sik/fcstock/model/vo/FcStockName.java
+++ sik/src/main/java/org/ict/sik/fcstock/model/vo/FcStockName.java
@@ -2,55 +2,9 @@ package org.ict.sik.fcstock.model.vo;
 
 public class FcStockName extends FcStock{
 	
-	private String fcName; //매장명
-	private String itemId; //상품ID
-	private String fcId; //가맹점ID
-	private String fcStockQuan; //수량
-	private String fcStockEtc; //비고
+	private String fcName;
+	private String itemName;
 	
-	public String getFcName() {
-		return fcName;
-	}
-	public void setFcName(String fcName) {
-		this.fcName = fcName;
-	}
-	public String getItemId() {
-		return itemId;
-	}
-	public void setItemId(String itemId) {
-		this.itemId = itemId;
-	}
-	public String getFcId() {
-		return fcId;
-	}
-	public void setFcId(String fcId) {
-		this.fcId = fcId;
-	}
-	public String getFcStockQuan() {
-		return fcStockQuan;
-	}
-	public void setFcStockQuan(String fcStockQuan) {
-		this.fcStockQuan = fcStockQuan;
-	}
-	public String getFcStockEtc() {
-		return fcStockEtc;
-	}
-	public void setFcStockEtc(String fcStockEtc) {
-		this.fcStockEtc = fcStockEtc;
-	}
-	public FcStockName(String fcName, String itemId, String fcId, String fcStockQuan, String fcStockEtc) {
-		super();
-		this.fcName = fcName;
-		this.itemId = itemId;
-		this.fcId = fcId;
-		this.fcStockQuan = fcStockQuan;
-		this.fcStockEtc = fcStockEtc;
-	}
-	@Override
-	public String toString() {
-		return "FcStockName [fcName=" + fcName + ", itemId=" + itemId + ", fcId=" + fcId + ", fcStockQuan="
-				+ fcStockQuan + ", fcStockEtc=" + fcStockEtc + "]";
-	}
 	public FcStockName() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -58,6 +12,27 @@ public class FcStockName extends FcStock{
 	public FcStockName(String itemId, String fcId, String fcStockQuan, String fcStockEtc) {
 		super(itemId, fcId, fcStockQuan, fcStockEtc);
 		// TODO Auto-generated constructor stub
+	}
+	public String getFcName() {
+		return fcName;
+	}
+	public void setFcName(String fcName) {
+		this.fcName = fcName;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public FcStockName(String fcName, String itemName) {
+		super();
+		this.fcName = fcName;
+		this.itemName = itemName;
+	}
+	@Override
+	public String toString() {
+		return "FcStockName [fcName=" + fcName + ", itemName=" + itemName + "]";
 	}
 	
 	
